66914692ca0db8e90ace448e6cfa3a1d6de03207
NDT2000 <nayan200000@gmail.com>
2026-04-22 15:30:48 -0400

    JWT services code setup done

---
+++ src/main/java/com/nayan/finance_tracker/dto/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.nayan.finance_tracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+    private String email;
+    private String fullName;
+    
+}
---
+++ src/main/java/com/nayan/finance_tracker/dto/RegisterRequest.java
@@ -0,0 +1,22 @@
+package com.nayan.finance_tracker.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @Size(min = 6, message = "Passwords must be atleast 6 characters")
+    @NotBlank(message = "Password is required")
+    private String password;
+    
+}
